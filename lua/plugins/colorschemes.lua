a064426968403bba8fe2e36561ac737a2828cb6e
Miitto <wpbickerton@gmail.com>
2025-01-18 18:35:18 +0000

    Transparent

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,9 +1,22 @@
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "catppuccin",
+    opts = {
+      transparent_background = true,
+      styles = {
+        conditionals = {},
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
-  }
+  },
 }
